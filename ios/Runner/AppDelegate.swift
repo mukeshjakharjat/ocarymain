cad5fdf07746200fd3ff4bf2efd5b83db6191ace
mukesh Kumar <Mukeshjakharjat@gmail.com>
2022-05-02 10:05:21 +0530

    Initial commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    var flutter_native_splash = 1
+    UIApplication.shared.isStatusBarHidden = false
+    GMSServices.provideAPIKey("AIzaSyD6ZsJ8Cidz1OhTeHZa2IOkz9G35xtVV-s")
+    GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
